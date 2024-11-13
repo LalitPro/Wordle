8407f8d85eb7ebe82b5f21950916e75493ea5ba4
LalitPro <140280rakesh@gmail.com>
2024-11-13 21:18:49 +0530

    Update hooks.js

--- src/wordle/hooks.js
+++ src/wordle/hooks.js
@@ -52,19 +52,14 @@ export function useOnSubmitGuess() {
 }
 
 export function usePickRandomWord() {
+export function usePickRandomWord() {
   const [_, setWord] = useRecoilState(selectedWordAtom);
 
-  /*
   useEffect(() => {
     const words = allWords;
     const index = Math.floor(Math.random() * words.length);
     setWord(words[index]);
   });
-  */
 
-  const randomWord = axios.get(
-    "https://random-word-api.herokuapp.com/word?length=5"
-  );
-  setWord(randomWord);
-  console.log(_);
+  // for cheating console.log(_);
 }
